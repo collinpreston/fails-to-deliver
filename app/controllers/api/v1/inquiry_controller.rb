e8b2ef5ba1f4d01206d98b4c25c82d86651e0659
curio-lighthouse <collin_preston@yahoo.com>
2021-12-22 08:31:45 -0500

    Update inquiry controller to return response message

--- app/controllers/api/v1/inquiry_controller.rb
+++ app/controllers/api/v1/inquiry_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::InquiryController < ApiController
   
     def create
         InquiryMailer.with(inquiry: params[:inquiry][:message], email: params[:email][:email]).inquiry_email.deliver_later
-        render status: :ok
+        render plain: {message: 'message sent'}.to_json, status: :ok, content_type: 'application/json'
     end
   
   end
