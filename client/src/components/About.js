987acd9e1a4d0952618e0c1691dab2cfc047259f
curio-lighthouse <collin_preston@yahoo.com>
2021-12-28 16:57:15 -0500

    Update paypal button

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -52,7 +52,7 @@ function About() {
                             <Accordion.Item eventKey="3">
                                 <Accordion.Header>Why is there not more historical data?</Accordion.Header>
                                 <Accordion.Body>
-                                    Cost.  We only make money through donations (<a href="https://www.paypal.com/donate/?hosted_button_id=UFQ23NBQ7YDJU">PayPal</a>).  Each report from the SEC can contain
+                                    Cost.  We only make money through donations (<a href="https://www.paypal.com/paypalme/collinhpreston">PayPal</a>).  Each report from the SEC can contain
                                     40k entries, which must be consumed and stored into our database.  Hosting and managing our
                                     current infrastructure costs $12 a month.  Adding more historically data will double or tripple
                                     that cost.
