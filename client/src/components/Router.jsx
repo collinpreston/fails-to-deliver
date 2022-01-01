4ec7afd9f06e6bece6d4f9b8cd3d4394af747db2
curio-lighthouse <collin_preston@yahoo.com>
2021-12-31 20:08:36 -0500

    update sitemap

--- client/src/components/Router.jsx
+++ client/src/components/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 
 export default (
     <Routes>
+        <Route path='/' />
         <Route path='/about' />
         <Route path='/contact' />
         <Route path='/api' />
