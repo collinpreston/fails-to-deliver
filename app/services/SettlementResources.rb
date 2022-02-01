554b182886cb109190ad6660f12ef73f7f68e270
curio-lighthouse <collin_preston@yahoo.com>
2022-02-01 08:11:22 -0500

    When uploading new resources, use the resource timestamp and not the local time.

--- app/services/SettlementResources.rb
+++ app/services/SettlementResources.rb
@@ -1,3 +1,4 @@
+require 'date'
 class SettlementResources
 
     attr_accessor :settlement_resources
@@ -8,8 +9,9 @@ class SettlementResources
 
     def create
         time = Time.new
-        settlement_period = SettlementPeriod.find_or_create_by!(month: time.month)
         @settlement_resources.each do |settlement|
+            date = Date.parse(settlement[:settlementDate])
+            settlement_period = SettlementPeriod.find_or_create_by!(month: date.month)
             company = Company.create_with(description: settlement[:description], data_last_updated: Time.now, cusip: settlement[:cusip]).find_or_create_by!(symbol: settlement[:symbol])
             Settlement.create!(settlement_date: settlement[:settlementDate], quantity: settlement[:quantity], price: settlement[:price], settlement_period_id: settlement_period.id, company_id: company.id)
         end
